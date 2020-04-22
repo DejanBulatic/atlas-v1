e76dc39ca3d265085d546108299f215c5a2bc53d
Dejan Bulatic <Dejanbulatic@outlook.com>
2020-04-22 20:46:39 +0200

    navigation js modification

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 //Mobile navigation slide
 const navSlide = () => {
+    //Variables
     const burgerDiv = document.getElementById("burger-div");
     const nav = document.getElementById("nav-links");
     const navLinks = document.querySelectorAll("#nav-links li");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+    const languages = document.getElementById("dropdown");
+    const dropIcon = document.getElementById("drop-icon");
 
-
+    //Events
     burgerDiv.onclick = () => {
         //Toggle nav
         nav.classList.toggle("nav-active");
+        //Language dropdown reset
+        dropdownMenu.classList.remove("drop-menu-active");
+        languages.classList.remove("dropdown-active");
+        dropIcon.classList.remove("fa-angle-up");
+        dropIcon.classList.add("fa-angle-down");
 
         //Animate links
         navLinks.forEach((link, index) => {
@@ -18,12 +27,7 @@ const navSlide = () => {
             }
         });
     };
-};
-//Dropdown menu for languages
-const dropMenu = () => {
-    const languages = document.getElementById("dropdown");
-    const dropdownMenu = document.getElementById("dropdown-menu");
-    const dropIcon = document.getElementById("drop-icon");
+
 
     languages.onclick = () => {
         //Showing/hiding language menu
@@ -41,9 +45,8 @@ const dropMenu = () => {
         }
 
     };
-
-
 };
 
+
+
 navSlide();
-dropMenu();
